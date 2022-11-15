29d979c7645d909be7f1780674938fcac44316a2
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2022-11-15 12:26:30 -1100

    fix-en las devoluciones de errores

--- src/controllers/aboutController.js
+++ src/controllers/aboutController.js
@@ -1,62 +1,72 @@
-const aboutServicio= require('../services/aboutService')
+const aboutServicio = require("../services/aboutService");
+const { endpointResponse } = require("../helpers/success");
 
-const get= async(req,res)=>{
-    try{      
-     const get= await aboutServicio.getAbout()
-    return res.status(200).send({about:get})
-    }   
-    catch(e){       
-        res.status(e?.status || 500)
-        .send({error:e?.message,error:"error"})
-    }
-  
- }
+const get = async (req, res) => {
+  try {
+    const about = await aboutServicio.getAbout();
+    endpointResponse({
+      res,
+      message: "about retrieved successfully",
+      body: about,
+    });
+  } catch (e) {
+    res.status(e?.status || 500).send({ error: e?.message, stack: e.stack });
+  }
+};
 
-const create= async(req,res)=>{
-    try{        
-        const model= {
-            texto:req.body.texto,
-            nombre:req.body.nombre,
-            imagen:req.body.imagen,
-            ciudad:req.body.ciudad,
-            cumple:req.body.cumple    
-        }        
-        const crear= await aboutServicio.create(model,res)
-    return res.status(200).send({"data":crear})
-    }   
-    catch(e){
-        res.status(500).send({error:e?.message,error:"error al crear c",stack:e.stack})
-    }
- }
+const create = async (req, res) => {
+  try {
+    const model = {
+      texto: req.body.texto,
+      nombre: req.body.nombre,
+      imagen: req.body.imagen,
+      ciudad: req.body.ciudad,
+      cumple: req.body.cumple,
+    };
+    const about = await aboutServicio.create(model, res);
+    endpointResponse({
+      res,
+      message: "about created successfully",
+      body: about,
+    });
+  } catch (e) {
+    res.status(e?.status || 500).send({ error: e?.message, stack: e.stack });
+  }
+};
 
- const update= async(req,res)=>{
-    try{
-        const {id}=req.params
-        const model =  {
-            texto:req.body.texto,
-            nombre:req.body.nombre,
-            imagen:req.body.imagen,
-            ciudad:req.body.ciudad,
-            cumple:req.body.cumple  
-        } 
-       
-    const crear= await aboutServicio.update(model,id)
-    return res.status(200).send({"data":crear})
-    }   
-    catch(e){
-        res.status(500).send({error:e?.message,error:"error al actualizar",stack:e.stack})
-    }
- }
-const Delete= async(req,res)=>{
-    try{
-        const {id}=req.params
-    const crear= await aboutServicio.Delete(id)
-    return res.status(200).send({"data":crear})
-    }
-   
-    catch(e){
-        res.status(500).send({error:e?.message,error:"error al eliminar",stack:e.stack})
-    }
- }
+const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const model = {
+      texto: req.body.texto,
+      nombre: req.body.nombre,
+      imagen: req.body.imagen,
+      ciudad: req.body.ciudad,
+      cumple: req.body.cumple,
+    };
 
- module.exports={create,get,update,Delete}
+    const about = await aboutServicio.update(model, id);
+    endpointResponse({
+      res,
+      message: "about updated successfully",
+      body: about,
+    });
+  } catch (e) {
+    res.status(e?.status || 500).send({ error: e?.message, stack: e.stack });
+  }
+};
+const Delete = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const about = await aboutServicio.Delete(id);
+    endpointResponse({
+      res,
+      message: "about deleted successfully",
+      body: about,
+    });
+  } catch (e) {
+    res.status(e?.status || 500).send({ error: e?.message, stack: e.stack });
+  }
+};
+
+module.exports = { create, get, update, Delete };
