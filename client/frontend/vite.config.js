0d5bd09a780adf8cbf8f4c3bb9284a26830b1abc
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2022-11-27 22:37:50 -1100

    feat-instalacion de react y servir index con express

---
+++ client/frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  /*  server: {
+    proxy: {
+      "/": "http://localhost:3000/",
+    },
+  }, */
+  plugins: [react()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: "./src/main.jsx",
+    },
+  },
+});
