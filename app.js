3cb73d161cd398b7b3c0497b51128cf042b87906
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2022-12-31 21:46:54 -1100

    feat-agrego axios frontd, cors, cambio de puerto local

--- app.js
+++ app.js
@@ -2,43 +2,43 @@ const express = require("express");
 const sequelize = require("./src/dataBase/connection");
 const { indexDb } = require("./src/dataBase/models/index");
 require("dotenv").config();
-const cors = require("cors");
+
 const indexRouter = require("./src/routes/index");
 const { apps } = require("./src/helpers/firebase");
 const path = require("path");
 const { route } = require("./src/routes/index");
+const cors = require("cors");
 const app = express();
+ app.use(
+  cors({
+    origin: ["http://localhost:8080"],
+  
+  })
+); 
 app.use(express.json());
-app.use("/", indexRouter);
-app.use("/", express.static(path.join(__dirname, "/client/frontend")));
 
-app.get("/*", (_req, res) => {
+app.use("/", indexRouter); 
+
+ app.use("/", express.static(path.join(__dirname, "publics/"))); 
+ app.use("/", express.static(path.join(__dirname, "/client/frontend"))); 
+
+app.get("/", (_req, res) => {
   res.sendFile(ruta.join(__dirname, "/client/frontend", "index.html"));
 });
-
+ 
 app.use((req, res, next) => {
   next(res.status(404).json({ error: "url not found" }));
 });
 
 // error handler
 app.use((err, req, res) => {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
 
-/* app.use(
-  cors({
-    credentials: true,
-    origin: ["http://127.0.0.1:5173/"],
-  })
-);
- */
-app.use(indexDb); // llamo a todos los modelos
+app.use(indexDb); 
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Servidor funcionando en el puerto ${process.env.SERVER_PORT}`);
