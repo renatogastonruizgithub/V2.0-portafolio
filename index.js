9680fa623d23a573dc48d53a54470bef5e048458
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2023-01-01 22:34:24 -1100

    fix-comentario produccion

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res) => {
+/* app.use((err, req, res) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
   res.render("error");
-});
+}); */
 
 app.use(indexDb); 
 
